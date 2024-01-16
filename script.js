91fbbe2f5b132212bd9ae273f862083e7e3d1fd9
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-16 19:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ function handleInput(input) {
+      // On typing a number, focus on the next input
+      if (input.value && input.nextElementSibling) {
+        input.nextElementSibling.focus();
+      }
+    }
+
+    function handleKeyDown(input) {
+      // On backspace key press, delete the number and focus on the previous input
+      if (event.key === 'Backspace' && input.value === '' && input.previousElementSibling) {
+        input.previousElementSibling.focus();
+      }
+    }
